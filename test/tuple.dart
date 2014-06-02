91cef109b801353826cb118ee336c0ac32608ff1
Sindre Føring Devik <sindre.devik@gmail.com>
2014-06-02 13:02:47 +0200

    added equality test

--- test/tuple.dart
+++ test/tuple.dart
@@ -1,2 +1,12 @@
 import 'package:unittest/unittest.dart';
 import 'package:tuple/tuple.dart';
+
+void main() {
+  var a = new Tuple(1, "hello");
+  var b = new Tuple(2, "hello");
+  var c = new Tuple("hello", 1);
+  var d = new Tuple(1, "hello");
+  expect(a == b, isFalse);
+  expect(a == c, isFalse);
+  expect(a == d, isTrue);
+}
